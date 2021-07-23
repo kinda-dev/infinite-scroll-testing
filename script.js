b72dd044b3cbab4a58c59a130f2884210bcfa5af
kinda-dev <maybe.fabio@gmail.com>
2021-07-23 15:07:40 -0700

    implement logic for input data end

--- script.js
+++ script.js
@@ -3,24 +3,40 @@ const container = document.querySelector('#container');
 const data = [...Array(53).keys()];
 
 
-let total = 5;
+let total = 0;
 let start = 0;
-function renderEntries() {
-    const entriesUl = document.createElement('ul');
-    for (let i = start; i < total; i++) {
-        const singleEntry = document.createElement('li');
-        singleEntry.setAttribute('class', 'entry');
-        singleEntry.textContent = `entry n' ${data[i]}`;
-        entriesUl.appendChild(singleEntry);
-    }
-    entriesUl.setAttribute('id', `nodeNumber${total}`);
-    container.appendChild(entriesUl);
-    total += 5;
-    start += 5;
+
+function hasMoreEntries(total, data) {
+    return total === 0 || start < data.length;
 }
-console.log(total)
-renderEntries();
-console.log(total)
-renderEntries();
-console.log(total)
+
+function renderEntries() {
+
+    if (hasMoreEntries(total, data)) {
+
+        const entriesUl = document.createElement('ul');
+        for (let i = start; i < total + 5; i++) {
+            if (data[i]) {
+
+                const singleEntry = document.createElement('li');
+                singleEntry.setAttribute('class', 'entry');
+                singleEntry.textContent = `entry n' ${data[i]}`;
+                entriesUl.appendChild(singleEntry);
+            }
+        }
+        entriesUl.setAttribute('id', `nodeNumber${total}`);
+        container.appendChild(entriesUl);
+        total += 5;
+        start += 5;
+
+    }
+
+}
+
+for (let i = 0; i < 11; i++) {
+
+    renderEntries();
+    console.log(total)
+}
+
 
