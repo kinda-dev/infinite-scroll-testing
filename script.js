f1aa84c90d4ca76fd1ea1230bd4d12163a313d9b
kinda-dev <maybe.fabio@gmail.com>
2021-07-23 14:10:23 -0700

    set up test array and render function

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const container = document.querySelector('#container');
+
+const data = [...Array(53).keys()];
+
+
+let total = 5;
+let start = 0;
+function renderEntries() {
+    const entriesUl = document.createElement('ul');
+    for (let i = start; i < total; i++) {
+        const singleEntry = document.createElement('li');
+        singleEntry.setAttribute('class', 'entry');
+        singleEntry.textContent = `entry n' ${data[i]}`;
+        entriesUl.appendChild(singleEntry);
+    }
+    entriesUl.setAttribute('id', `nodeNumber${total}`);
+    container.appendChild(entriesUl);
+    total += 5;
+    start += 5;
+}
+console.log(total)
+renderEntries();
+console.log(total)
+renderEntries();
+console.log(total)
+
